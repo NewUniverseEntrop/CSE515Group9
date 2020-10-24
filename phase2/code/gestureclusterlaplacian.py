4dc55c64c1b7c93fb5ec09ab4fdc62494f980b73
Xin Xu <xinxu@Xins-MacBook-Pro.local>
2020-10-23 18:51:35 -0700

    spectral clustering, smallest k eigenvalues

--- phase2/code/gestureclusterlaplacian.py
+++ phase2/code/gestureclusterlaplacian.py
@@ -27,34 +27,38 @@ else:
     print('wrong clustering option')
 
 testmatrix = np.zeros((10, 10))
-for i in range(len(testmatrix)):
-    for j in range(i, len(testmatrix)):
-        testmatrix[i, j] = (j + 1) * 0.1
-        testmatrix[j, i] = (j + 1) * .1
+for i in range(len(testmatrix) // 2):
+    for j in range(len(testmatrix) // 2):
+        testmatrix[i, j] = 1
+        testmatrix[j, i] = 1
+for i in range(len(testmatrix) // 2, len(testmatrix)):
+    for j in range(len(testmatrix) // 2, len(testmatrix)):
+        testmatrix[i, j] = 1
+        testmatrix[j, i] = 1
 
 
-# print(testmatrix)
+#print(testmatrix)
 
 
-def gesturecluster(matrix, p, k=2, knear=4):
+def gesturecluster(matrix, k=2):
     if not matrix.shape[0] == matrix.shape[1]:
         print('matrix is not a square')
     G = networkx.Graph()
     for i in range(len(matrix)):
         G.add_node(str(i))
     for i in range(len(matrix)):
-        subvector = matrix[i, :]
-        knearest = np.argpartition(subvector, -knear)[-knear - 1:-1]  # k nearest
-        for j in knearest:
-            if subvector[j] > 0:  # graph weight need to be positive
-                G.add_edge(str(i), str(j), weight=subvector[j])  # build k-nearest graph
+        #subvector = matrix[i, :]
+        #knearest = np.argpartition(subvector, -knear)[-knear - 1:-1]  # k nearest
+        for j in range(len(matrix)):
+            if matrix[i][j] > 0:  # graph weight need to be positive
+                G.add_edge(str(i), str(j), weight=matrix[i][j])  # build k-nearest graph
     normalaplacian = networkx.normalized_laplacian_matrix(G)
-    kvals, kvecs = eigs(normalaplacian, k)
-    V = np.mat(kvecs).real  # k*n matrix for cluster
+    kvals, kvecs = eigs(normalaplacian, k, which = 'SM')
+    V = abs(np.mat(kvecs))  # k*n matrix for cluster
     kmeans = KMeans(n_clusters=k, random_state=0).fit(V)
     clusterresult = kmeans.labels_
-    print(clusterresult)
+    #print(clusterresult)
     return clusterresult
 
 
-gesturecluster(testmatrix, 5)  # for debugging only, no real meaning
+gesturecluster(testmatrix, 2)  # for debugging only, no real meaning
